63788e748c14c5fa16f2e4e86c444bab41b3345e
awkale <hi@awkale.me>
2013-04-10 01:39:13 -0400

    initial commit

---
+++ html/11/cookie-monster/js/app.js
@@ -0,0 +1,7 @@
+$(function(Event){
+	$("#startSwapping").click(function(Event){
+		$("body").css("background-image", "url(\"images/bg1.jpg\")");
+		$("h2").html("Peanut Butter Cookie Evangelist");
+	});
+
+});
