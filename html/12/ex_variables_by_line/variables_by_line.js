43ffede4e342eec04c6c94c782751f7628a9939c
awkale <hi@awkale.me>
2013-04-11 18:59:36 -0400

    added class 12 files

---
+++ html/12/ex_variables_by_line/variables_by_line.js
@@ -0,0 +1,17 @@
+$(
+  //  When the page is ready to be used, we need to respond to that
+  //   so we write an anonymous function (the listener function)
+  //   so that jQuery can do something  when the page is finished loading. 
+
+	function(event) {
+	
+		//Replace the words "Replace_Me" then run index.html and view the console.  
+
+		var first_name = "Replace_Me";
+		var last_name = "Replace_Me";
+		
+		console.log(first_name);
+		console.log(last_name);
+		console.log("I <3 JavaScript");
+  }
+);
