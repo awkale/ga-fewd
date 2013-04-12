2f8dbb88e91a38b77c9de68e2711e546b4334ea7
awkale <hi@awkale.me>
2013-04-11 21:31:16 -0400

    completed in class assignments

--- html/12/coa_conditionals/conditionals.js
+++ html/12/coa_conditionals/conditionals.js
@@ -5,33 +5,42 @@ $(function(){
 	 * this is a multi-line comment
 	 */
 
-	 //Prompt the user for their name and last name. 
-
+	 //Prompt the user for their name and last name.
+	 var first = prompt('What is your first name?');
+	 var last = prompt('What is your last name?', 'Last Name');
 
 	//Create a new variable called full name and store the users full name.
-	
-	
+	var fullname = first + ' ' + last;
+
 	//Print the full name to the console.
-	
+	console.log('Your full Name ' + fullname);
 
 	//Prompt the user for their age.
-	
+	var age = prompt('What is your age?', 'enter age');
 
 
 	//Add 10 to the age and print the output to the console.
-	
-	
-	
+	age = parseInt(age);
+	console.log('Your real age is ' + (age + 10));
+
+
 	//Verify that the user is over 18 and print if they are a minor or adult to the console.
 
+	if(age < 18){
+		console.log('You are a minor. No lotto ticket for you');
+	}
+	else{
+		console.log('You are an adult.');
+	}
 
 
 
 
-	
 	//Verify if the first name is "General" and the last name is NOT "Assembly"
 
-
+	if((first == 'general') && (last != 'assembly')){
+		console.log('you are the winner');
+	}
 
 
 });
--- html/12/ex_line_by_line/line_by_line.js
+++ html/12/ex_line_by_line/line_by_line.js
@@ -1,4 +1,8 @@
 /*
-Write three statements that prints your first name, 
+Write three statements that prints your first name,
 last name and your favorite programming language thus far to the console.
-*/  
+*/
+
+console.log("Alex");
+console.log("Kale");
+console.log("PHP");
