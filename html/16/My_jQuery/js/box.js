17c5b64a53fa381c4801eec75f52713992d214be
awkale <hi@awkale.me>
2013-04-26 17:00:20 -0400

    completed in class exercises

--- html/16/My_jQuery/js/box.js
+++ html/16/My_jQuery/js/box.js
@@ -3,3 +3,18 @@
 */
 
 
+$(function(){
+	var domchange;
+	do {
+	 	domchange = prompt("Do you want to change the DOM, (Y)es or (N)o", "(Y) or (N)");
+	 	if (domchange == 'Y') {
+			$('.box').hide(5000);
+			$('.box2').show(2000);
+			$('.box2').css('background', 'blue');
+		} else if (domchange == "N") {
+		alert("I have nothing to do!");
+		}
+	} while ( domchange !=="Y" && domchange !=="N");
+
+
+});
--- html/16/grocery_list/js/grocery_list.js
+++ html/16/grocery_list/js/grocery_list.js
@@ -1,3 +1,7 @@
+$(function(){
+
+
+
 	var list = [];
 	var food_item;
 	var found = false;
@@ -17,7 +21,24 @@
 		found = false;
 		response = prompt('(a)dd item or (q)uit?');
 	}
+
+
+
+	$('p').remove();
+	$('h1').html('My Shopping List');
+	$('h1').after('<ul></ul>');
+
+
+	for (var i=0; i < list.length; i++){
+			$('ul').append('<li>'+ list[i] + '</li>');
+	}
+
+
+
 	console.log('My Shopping List');
 	for (var i=0; i < list.length; i++){
 		console.log(list[i]);
 	}
+
+
+});
